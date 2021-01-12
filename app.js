9dede7f2a57d19647c88034294c5c5eab9d296d4
Trend20 <enockomondi305@gmail.com>
2021-01-12 05:26:47 +0300

    adding cdns

--- app.js
+++ app.js
@@ -2,85 +2,86 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // function to save the bookmarks
 
-function saveBookmark(event){
+function saveBookmark(event) {
+
+    // prevent default form behaviour
+    event.preventDefault();
 
     // grad form values
     let siteName = document.getElementById('siteName').value;
     let siteURL = document.getElementById('siteURL').value;
 
     // validation
-    if(!siteName || !siteURL){
+    if (!siteName || !siteURL) {
         alert('Please fill in the form');
     }
 
     // add the values to an object
-    const bookmark={
-        name:siteName,
-        url:siteURL,
+    const bookmark = {
+        name: siteName,
+        url: siteURL,
     };
     // test bookmarks
-    if(localStorage.getItem('bookmarks') === null){
+    if (localStorage.getItem('bookmarks') === null) {
         // init array
-        let bookmarks=[];
+        let bookmarks = [];
 
         // push items
         bookmarks.push(bookmark);
         // set to localstorage
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-    }else{
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
         // get an item
         let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         // add bookmark to array
         bookmarks.push(bookmark);
 
         // reset back to local storage
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
     }
     // refetch bookmarks
     fetchBookmarks();
-    // prevent default form behaviour
-    event.preventDefault();
 }
 // deletebookmarks
-function deleteBookmark(url){
+function deleteBookmark(url) {
     // get bookmarks from local storage
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     // loop through
-    for(let i = 0; i < bookmarks.length; i++){
-        if(bookmarks[i].url == url){
+    for (let i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
             // remove from array
             bookmarks.splice(i, 1);
         }
     }
-     localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     //  refetch bookmarks
     fetchBookmarks();
 }
 
 // fetchBookmarks
 
-function fetchBookmarks(){
+function fetchBookmarks() {
     // get bookmarks from the localStorage
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    
+
     // output id
-    let bookmarksResults= document.getElementById('bookmarksResults');
+    let bookmarksResults = document.getElementById('bookmarksResults');
 
     // build output
 
     bookmarksResults.innerHTML = '';
 
-    for(let i=0; i < bookmarks.length; i++){
+    for (let i = 0; i < bookmarks.length; i++) {
         let name = bookmarks[i].name;
         let url = bookmarks[i].url;
 
-        bookmarksResults.innerHTML += '<div class="well">'+
-                                     '<h3>'+name+
-                                     '<a class="btn bnt-default" target="_blank" href="'+url+'">Visit</a>'+
-                                     '<a onclick="deleteBookmark(\''+url+'\')" class="btn bnt-danger"  href="#">Delete</a>'
-                                     '</h3>'+
-                                     '</div>'
+        bookmarksResults.innerHTML += '<div class="well">' +
+            '<h3>' + name +
+            '<a class="btn bnt-default" target="_blank" href="' + url + '">Visit</a>' +
+            '<a onclick="deleteBookmark(\'' + url + '\')" class="btn bnt-danger"  href="#">Delete</a>'
+        '</h3>' +
+        '</div>'
 
     }
 }
